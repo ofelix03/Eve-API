a72618b14bde39f2e56f5d7fad7f91d0d22091cf
Felix Otoo <ofelix03@gmail.com>
2020-02-17 16:20:51 +0000

    [ADD] update db_config.py

--- db_config.py
+++ db_config.py
@@ -7,5 +7,6 @@
 # session = Session()
 
 SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@db:5432/Eve2'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@127.0.0.1:5432/Eve2'
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
